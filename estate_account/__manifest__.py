cb530c7942f0a0995cb11081dd8066d32d4e483b
ywindows98 <tkachenko.iwn@gmail.com>
2025-06-22 21:52:31 +0200

    Estate_Account: Add the module that extends the real_estate module with invoicing functionality.
    estate_property.py: inherit the model from the real_estate module and extend the sell_estate_property_action method to create an account.move invoice when the property is sold. Invoice includes 6% service fees and 100$ of administrative fees.

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,14 @@
+
+
+{
+    'name': 'Estate Account Link',
+    'depends': ['real_estate', 'account'],
+    'version': '0.1',
+    'summary': 'Link module for Real Estate and Account.',
+    'author': 'ywindows98',
+    'application': True,
+    'category': 'Real Estate',
+    'data': [
+
+    ],
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,39 @@
+from odoo import models, fields, api, _, Command
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools.float_utils import float_compare, float_is_zero
+
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+class EstateProperty(models.Model):
+    _inherit = 'estate.property'
+
+    def sell_estate_property_action(self):
+        invoice_vals_list = []
+
+        invoice_vals = {
+            'name': f'{self.name} sell invoice',
+            'move_type': 'out_invoice',
+            'partner_id': self.partner_id.id,
+            'invoice_line_ids': []
+        }
+
+        invoice_line_vals = [
+            Command.create({
+                "name": "Service fees",
+                "quantity": 1,
+                "price_unit": self.selling_price * 0.06  # 6% of the selling price as a service fee
+            }),
+            Command.create({
+                "name": "Administrative fees",
+                "quantity": 1,
+                "price_unit": 100  # 100 as the administrative fees
+            })
+        ]
+
+        invoice_vals['invoice_line_ids'] += invoice_line_vals
+        invoice_vals_list.append(invoice_vals)
+
+        self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice_vals_list)
+
+        return super().sell_estate_property_action()
