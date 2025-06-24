18c59b26bb4a3fff7177e1c46c2eefb365452dbf
ywindows98 <tkachenko.iwn@gmail.com>
2025-06-24 16:25:13 +0200

    Awesome_Owl: Create a Counter template (js + xml).

--- awesome_owl/static/src/main.js
+++ awesome_owl/static/src/main.js
@@ -3,6 +3,7 @@
 import { browser } from "@web/core/browser/browser";
 import { mount, whenReady } from "@odoo/owl";
 import { Playground } from "./playground";
+import { Counter } from "./counter/counter";
 import { templates } from "@web/core/assets";
 
 // Mount the Playground component when the document.body is ready
