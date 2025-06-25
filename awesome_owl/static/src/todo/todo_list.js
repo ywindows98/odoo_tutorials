e0c4d6dbec6a10f6c4122ddce06e6cb4bf390cb5
ywindows98 <tkachenko.iwn@gmail.com>
2025-06-25 20:01:19 +0200

    Awesome_Owl: Implement a basic ToDo list component and a ToDo item component.

---
+++ awesome_owl/static/src/todo/todo_list.js
@@ -0,0 +1,17 @@
+/** @odoo-module **/
+
+import { Component, useState } from "@odoo/owl";
+import { TodoItem } from "./todo_item";
+
+export class TodoList extends Component {
+    static template = "awesome_owl.TodoList";
+    static components = { TodoItem };
+
+    setup() {
+        this.todos = useState([
+            { id: 3, description: "buy milk", isCompleted: false },
+            { id: 4, description: "sell milk", isCompleted: true }
+        ]);
+    }
+
+}
