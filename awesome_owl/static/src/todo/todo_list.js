56e68e3411c1c1d2b258b3abba21e921935906a7
ywindows98 <tkachenko.iwn@gmail.com>
2025-06-25 22:20:57 +0200

    Awesome_Owl: Implement adding a todo item through input.

--- awesome_owl/static/src/todo/todo_list.js
+++ awesome_owl/static/src/todo/todo_list.js
@@ -8,10 +8,24 @@ export class TodoList extends Component {
     static components = { TodoItem };
 
     setup() {
-        this.todos = useState([
-            { id: 3, description: "buy milk", isCompleted: true },
-            { id: 4, description: "sell milk", isCompleted: false }
-        ]);
+//        this.todos = useState([
+//            { id: 3, description: "buy milk", isCompleted: true },
+//            { id: 4, description: "sell milk", isCompleted: false }
+//        ]);
+        this.nextId = 0;
+        this.todos = useState([]);
+    }
+
+    addTodo(ev) {
+        if(ev.keyCode === 13 && ev.target.value != ""){
+            this.todos.push({
+                id: this.nextId++,
+                description: ev.target.value,
+                isCompleted: false
+            })
+
+            ev.target.value = "";
+        }
     }
 
 }
