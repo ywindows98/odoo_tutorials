e90698ce77ff556e8614f390ce26865eb8fc6d03
ywindows98 <tkachenko.iwn@gmail.com>
2025-06-26 10:51:57 +0200

    Awesome_Owl: Change a card component to be more generic, add default slot, to allow the use of other components in it.

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -7,6 +7,12 @@ export class Card extends Component {
 
     static props = {
         title: {type: String},
-        content: {type: String}
+
+        slots: {
+            type: Object,
+            shape: {
+                default: true
+            },
+        }
     };
 }
