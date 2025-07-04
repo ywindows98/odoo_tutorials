d930eb527881ce1eed0864e2e02c3ec68aaf62fc
ywindows98 <tkachenko.iwn@gmail.com>
2025-07-04 15:42:34 +0200

    Real_Estate: Tests: Create RealEstateTestCommon with initial data for tests.

---
+++ real_estate/tests/common.py
@@ -0,0 +1,43 @@
+from odoo import fields, Command
+from odoo.tests.common import TransactionCase, HttpCase, tagged, Form
+
+import json
+import time
+import base64
+from lxml import etree
+from unittest import SkipTest
+
+
+class RealEstateTestCommon(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.simple_company = cls.env['res.company'].create({
+            'name': 'simple company'
+        })
+
+        cls.simple_agent = cls.env['res.users'].create({
+            'name': 'simple agent',
+            'login': 'simple_agent',
+            'password': 'simple_agent',
+            'company_id': cls.simple_company.id,
+            'groups_id': [
+                Command.link(cls.env.ref('real_estate.group_real_estate_user').id)
+            ],
+        })
+
+        cls.sold_property_sample = cls.env['estate.property'].create({
+            'name': 'Sold Property Test',
+            'expected_price': 144000,
+            'user_id': cls.simple_agent.id,
+            'company_id': cls.simple_agent.company_id.id,
+            'state': 'sold'
+        })
+
+        cls.property_with_no_offers = cls.env['estate.property'].create({
+            'name': 'Sold Property Test',
+            'expected_price': 112000,
+            'user_id': cls.simple_agent.id,
+            'company_id': cls.simple_agent.company_id.id
+        })
